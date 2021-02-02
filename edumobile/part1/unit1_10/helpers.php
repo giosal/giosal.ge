2b8d34c496ddde512cc2c8525e0758697024b481
George Salukvadze <giosal90@gmail.com>
2021-02-02 18:42:40 +0100

    Delete edumobile/part1 directory

--- edumobile/part1/unit1_10/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function pageTop() {
-        echo "<html><head><title>Require and String functions</title></head><body>";
-    }
-
-    function pageBottom() {
-        echo "</body></html>";
-    }
-?>
