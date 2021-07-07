5c0145f2b95a715fb462d36edf40a2447592ab17
George Salukvadze <giosal90@gmail.com>
2021-07-07 16:44:44 +0200

    v. 2.0.6 beta - Removed API key from repository

--- map_challenge/get_data.php
+++ map_challenge/get_data.php
@@ -1,7 +1,7 @@
 <?php
 $latitude = $_POST["lat"];
 $longitude = $_POST["long"];
-$url = "https://api.openweathermap.org/data/2.5/weather?lat=" . $latitude . "&lon=" . $longitude . "&appid=f402bce671988768fddb242f8dad1a4c&units=metric";
+$url = "https://api.openweathermap.org/data/2.5/weather?lat=" . $latitude . "&lon=" . $longitude . "&appid={APIKey}&units=metric";
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($curl);
